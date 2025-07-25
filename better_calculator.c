97cf8987c62e819ca8767ef39d27f5989e0a4d71
roshankrsu <roshankrsu@email.com>
2025-07-25 21:56:05 +0530

    calculatot that can do DMAS

---
+++ better_calculator.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  
+    double num1;
+    double num2;
+    char op;//operator
+
+    printf("Enter a Number: ");
+    scanf("%lf",&num1);
+    printf("Enter a Operator: ");
+    scanf(" %c", &op);
+    printf("Enter a Number: ");
+    scanf("%lf",&num2);
+
+    if(op == '+'){
+        printf("%.2f", num1 + num2);
+    }else if(op == '-'){
+        printf("%.2f", num1 - num2);
+    }else if(op == '*'){
+        printf("%.2f", num1 * num2);
+    }else if(op == '/'){
+        printf("%.2f", num1 / num2);
+    }else{
+        printf("Invalid operators");
+    }
+
+
+    return 0;
+}
